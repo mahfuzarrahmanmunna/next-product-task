0fd709f2b1ef81a78018642bc1fb999025ad8e99
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-08-21 23:27:40 +0600

    added and work new many of commit

--- app/actions/auth/registerUser.js
+++ app/actions/auth/registerUser.js
@@ -1,1 +1,22 @@
+"use server"
+import bcrypt from 'bcrypt'
+import { collectionsNameObj, dbConnect } from "@/app/lib/dbConnect"
 
+export const registerUser = async (payload) => {
+    const userCollection = dbConnect(collectionsNameObj().userCollection)
+
+    const { name, email, password } = payload
+    if (!email, !password) {
+        return { success: false }
+    }
+
+    const user = await userCollection.findOne({ email: payload.email })
+    if (!user) {
+        const hashedPassword = await bcrypt.hash(password, 10)
+        payload.password = hashedPassword
+        const result = await userCollection.insertOne(payload)
+        const { acknowledged, insertedId } = result;
+        return { acknowledged, insertedId }
+    }
+    return { success: false }
+}
